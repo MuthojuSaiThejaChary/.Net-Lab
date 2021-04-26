6adb1c677069ae068bb41add666044753f4603a1
SaithejaMuthoju <muthojusaitheja@gmail.com>
2021-04-26 13:09:27 +0530

    lab12 task1

---
+++ Lab12/Lab12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab12
+{
+    class Program
+    {
+        public static void Read(string file_path)
+        {
+            try
+            {
+                
+                if(File.Exists(file_path))
+                {
+                   string s= File.ReadAllText(file_path);
+                    Console.WriteLine(s);
+                }
+                else
+                {
+                    Console.WriteLine("File Not Exists");
+                }
+            }
+            catch(IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void Main()
+        {
+            Console.Write("Enter the path of the file along with filename:");
+            string filepath = Console.ReadLine();
+            Read(filepath);
+            Console.ReadLine();
+
+        }
+    }
+}
